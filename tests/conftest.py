c0ead0fc40014837810aa72c155d3117ebfc667c
Alexey Pasyuk <apasyuk@netgate.com>
2020-06-07 19:39:20 +0400

    refactored conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,25 @@
 from mainflux.app import MainfluxApp
 import pytest
 import uuid
-import requests
 import httpx
-from .common import THINGS_TO_DELETE, CHANNELS_TO_DELETE, APP
+from .common import THINGS_TO_DELETE, CHANNELS_TO_DELETE
+
+APP_KWARGS = {
+    'url': "your_ip_to_mainflux",
+    'port': "80",
+    'user': "your_user_email",
+    'password': "your_password"
+}
 
 
 class TestManager:
+    def __init__(self, **app_args):
+        self.app = MainfluxApp(url=app_args['url'],
+                               port=app_args['port'],
+                               user_email=app_args['user'],
+                               user_password=app_args['password'])
 
-    app = None
-
-    def __new__(cls):
+    def __new__(cls, **kwargs):
         if not hasattr(cls, 'instance'):
             cls.instance = super(TestManager, cls).__new__(cls)
         return cls.instance
@@ -18,10 +27,8 @@ class TestManager:
 
 @pytest.fixture(scope='module')
 def app():
-    app = MainfluxApp()
-    print(f"Api token: {app.api.token}")
-    test_manager = TestManager()
-    test_manager.app = app
+    test_manager = TestManager(**APP_KWARGS)
+    app = test_manager.app
     return app
 
 
@@ -42,7 +49,7 @@ def random_thing_name():
 @pytest.fixture(scope='module')
 def clean_things():
     yield
-    app = TestManager().app
+    app = TestManager(**APP_KWARGS).app
     for thing in THINGS_TO_DELETE:
         app.api.delete_thing(thing)
 
@@ -50,6 +57,6 @@ def clean_things():
 @pytest.fixture(scope='module')
 def clean_channels():
     yield
-    app = TestManager().app
+    app = TestManager(**APP_KWARGS).app
     for channel in CHANNELS_TO_DELETE:
         app.api.delete_thing(channel)
